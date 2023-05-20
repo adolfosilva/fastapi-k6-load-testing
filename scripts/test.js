0d08509bce540cfa58f7854e65e862ef59846cca
Adolfo Silva <code@adolfosilva.org>
2023-05-20 23:11:49 +0100

    Relax threshold requirements

--- scripts/test.js
+++ scripts/test.js
@@ -7,7 +7,7 @@ export const options = {
     duration: '10s',
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-        http_req_duration: ['p(95)<5'], // 95% of requests should be below 200ms
+        http_req_duration: ['p(95)<10'], // 95% of requests should be below 10ms
     },
 };
 
